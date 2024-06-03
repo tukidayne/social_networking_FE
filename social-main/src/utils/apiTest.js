2b32de1f34af31f4baaf3865eb02d29fae17bfc8
tukidayne <95684790+tukidayne@users.noreply.github.com>
2024-06-04 00:03:10 +0700

    Delete social-main directory

--- social-main/src/utils/apiTest.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export default function ApiCallerTest(method = "GET", url, data) {
-  return axios({
-    method: method,
-    url: `http://localhost:3000${url}`,
-    data: data,
-  });
-}
